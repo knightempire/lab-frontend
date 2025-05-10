ab8cbb5a11b50e7ce461daf72e73c403c8090990
Santhosh A S <santhosh292k@gmail.com>
2025-05-10 11:00:46 +0530

    Added logo,icon,favicon and updated search functionality

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,45 +1,20 @@
-'use client';
-
-import { useState } from 'react';
-import { usePathname } from 'next/navigation';
-import Sidebar from '../components/Sidebar';
-import Navbar from '../components/Navbar';
 import './globals.css';
+import ClientLayout from '../components/ClientLayout';
 
-const validPaths = [
-  '/',
-  '/admin/dashboard',
-  '/admin/product',
-  '/admin/request',
-  '/admin/users',
-  '/user/product',
-];
+export const metadata = {
+  title: 'Amuda Lab Management',
+  icons: {
+    icon: {
+      url: '/images/icon.png',
+    },
+  },
+};
 
 export default function RootLayout({ children }) {
-  const pathname = usePathname();
-  const [sidebarOpen, setSidebarOpen] = useState(true);
-
-  const toggleSidebar = () => {
-    setSidebarOpen((prev) => !prev);
-  };
-
-  const isAuthRoute = pathname.startsWith('/auth') || pathname.endsWith('/404');
-  const isValidRoute = validPaths.includes(pathname);
-
   return (
     <html lang="en" className="h-full">
-      <body className={isAuthRoute || !isValidRoute ? '' : 'flex h-full bg-gray-50 overflow-hidden'}>
-        {isAuthRoute || !isValidRoute ? (
-          children
-        ) : (
-          <>
-            <Sidebar isOpen={sidebarOpen} />
-            <div className="flex flex-col flex-1">
-              <Navbar toggleSidebar={toggleSidebar} />
-              <main className="flex-1 overflow-y-auto">{children}</main>
-            </div>
-          </>
-        )}
+      <body className="h-full">
+        <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
   );
