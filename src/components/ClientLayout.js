ef6ef2b64bf1cf8a77486e7752024fbded7238b8
Abinesh <108071239+knightempire@users.noreply.github.com>
2025-05-17 00:17:05 +0530

    footer

--- src/components/ClientLayout.js
+++ src/components/ClientLayout.js
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { usePathname } from 'next/navigation';
 import Sidebar from './Sidebar';
 import Navbar from './Navbar';
+import Footer from './Footer';
 
 const validPaths = [
   '/',
@@ -32,6 +33,7 @@ export default function ClientLayout({ children }) {
       <div className="flex flex-col flex-1">
         <Navbar toggleSidebar={toggleSidebar} />
         <main className="flex-1 overflow-y-auto">{children}</main>
+        <Footer />
       </div>
     </div>
   );
