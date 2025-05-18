227b8f297fb7968e4584e77b1c637bd1ad476ac5
Abinesh T <108071239+knightempire@users.noreply.github.com>
2025-05-18 23:30:29 +0530

    Update page.js
    build error fix

--- src/app/admin/review/page.js
+++ src/app/admin/review/page.js
@@ -941,11 +941,12 @@ const AdminRequestViewContent = () => {
 export default function AdminRequestView() {
   return (
     <Suspense 
-    fallback={
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
-    </div>
-  }
+      fallback={
+        <div className="flex items-center justify-center min-h-screen">
+          <div className="w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+        </div>
+      }
+    >
       <AdminRequestViewContent />
     </Suspense>
   );
